f0619213de161c50e9e06268346d498d1eaa038e
ShAhilK2 <80386008+ShAhilK2@users.noreply.github.com>
2026-03-16 15:42:00 +0530

    feat 04 : Implemented a Profile Screen with user info and signout

--- components/screen.tsx
+++ components/screen.tsx
@@ -5,8 +5,15 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 const Screen = ({ children }: { children: React.ReactNode }) => {
   const insets = useSafeAreaInsets();
 
-  const platformPadding = Platform.OS === "ios" ? insets.top + 20 : insets.top;
-  return <View style={{ paddingTop: platformPadding }}>{children}</View>;
+  const platformPadding = Platform.OS === "ios" ? insets.top + 0 : insets.top;
+  return (
+    <View
+      style={{ paddingTop: platformPadding }}
+      className="flex-1 bg-background"
+    >
+      {children}
+    </View>
+  );
 };
 
 export default Screen;
